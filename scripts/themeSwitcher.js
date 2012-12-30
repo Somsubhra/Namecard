d6c5af2881bba7521d9dd44ea1dcd7a6d6682261
Somsubhra <somsubhra.bairi@gmail.com>
2012-12-30 15:56:24 +0530

    latest

--- scripts/themeSwitcher.js
+++ scripts/themeSwitcher.js
@@ -1,13 +1,12 @@
 $(function() { 
 	$("#themeSwitcher").click(function(){
-			if($("#themeSwitcher").html()=="Off"){
+			if($("#themeSwitcher").html()=="Lights Off!"){
 				$("#style").attr("href","style/negativeStyle.css");
-				$("#themeSwitcher").html("On");
+				$("#themeSwitcher").html("Lights On!");
 			}
 			else{
 				$("#style").attr("href","style/positiveStyle.css");
-				$("#themeSwitcher").html("Off");
-				
+				$("#themeSwitcher").html("Lights Off!");	
 			}
 	});
 });
